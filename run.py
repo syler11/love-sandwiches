f50c81c323e2584d61a17abad4d437481d2ad83c
syler11 <nemeth.szilard82@gmail.com>
2021-12-27 07:03:04 +0100

    function to update sales worksheet data

--- run.py
+++ run.py
@@ -50,4 +50,15 @@ def validate_data(values):
     
     return True
 
+def update_sales_worksheet(data):
+    """
+    Will update the sales worksheet data with a new row
+    """
+    print("Updating sales worksheet...\n")
+    sales_worksheet = SHEET.worksheet("sales")
+    sales_worksheet.append_row(data)
+    print("Sales worksheet updated successfully.\n")
+
 data = get_sales_data()
+sales_data = [int(num) for num in data]
+update_sales_worksheet(sales_data)
