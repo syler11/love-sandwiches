52cfc12bb648db85f19caea442be628d26fb8b3a
syler11 <nemeth.szilard82@gmail.com>
2021-12-27 11:18:47 +0100

    main fuction added

--- run.py
+++ run.py
@@ -59,6 +59,14 @@ def update_sales_worksheet(data):
     sales_worksheet.append_row(data)
     print("Sales worksheet updated successfully.\n")
 
-data = get_sales_data()
-sales_data = [int(num) for num in data]
-update_sales_worksheet(sales_data)
+def main():
+    """
+    Run all programs
+    """
+    data = get_sales_data()
+    sales_data = [int(num) for num in data]
+    update_sales_worksheet(sales_data)
+
+    print("Welcome to Love Sanswich Data automation")
+
+main()
